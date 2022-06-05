a7b2f81829227bd60a4152c18be52c0bfe900c02
Davi Serrano <daviscardoso2901@gmail.com>
2022-06-05 19:12:24 -0300

    numero add

---
+++ index.js
@@ -0,0 +1,9 @@
+const BtnaddNumber = document.getElementById("add-number");
+var boxNumbers = document.getElementById("box");
+
+function addNumber(){
+   boxNumbers.innerHTML += 
+   `<p class="numbers">
+    6
+    </p>` 
+}
