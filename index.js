e49a1b171d3862d918db9cd288079e0691e416b1
Davi Serrano <daviscardoso2901@gmail.com>
2022-06-17 22:51:23 -0300

    trabalhando no botão de sortear

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const BtnaddNumber = document.getElementById("add-number");
 var boxNumbers = document.getElementById("box");
+var numberofBox = 6
 
 function addNumber(){
-   boxNumbers.innerHTML += 
-   `<p class="numbers">
-    6
-    </p>` 
+    numberofBox ++
+
+    if(numberofBox > 9){
+        BtnaddNumber.style.display = "none"
+    }
+
+   boxNumbers.innerHTML += `<p class="numbers"> ${numberofBox} </p>`     
+}
+
+
+function sortButton(){
+    // const myNumbers = [5,15,20,25,30,35]
+    const Numbers = document.querySelectorAll(".numbers")
+
+    BtnaddNumber.style.display = "none"
+
+    Numbers.forEach( (numb)=>{
+        numb.innerText = "55"
+    })
+
 }
